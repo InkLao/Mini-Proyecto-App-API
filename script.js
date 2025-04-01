01d64c3d04e151afbc63d642e05f17739ab9b8f7
ArturoITSON <sergio.garcia233316@potros.itson.edu.mx>
2025-04-01 15:29:40 -0700

    Implementacion menor en libro recomendado

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
         
 let boton = document.getElementById("btn_consultar");
 
+
+const genres = [
+    "fiction", "mystery", "fantasy", "horror", "romance",
+    "science_fiction", "history", "biography", "philosophy",
+    "psychology", "self_help", "science", "technology", 
+    "art", "music", "poetry", "drama", "business", "health"
+];
+
+
+
 boton.onclick = function(){
  
     let busqueda = document.getElementById("txtBuscar").value;
@@ -33,3 +43,47 @@ boton.onclick = function(){
     })
 
 } 
+
+
+function randomBooks(){
+
+    const randomGenre = genres[Math.floor(Math.random() * genres.length)];
+    const url = `https://openlibrary.org/subjects/${randomGenre}.json?limit=50`;
+    console.log(randomGenre);
+
+    
+
+    fetch(url).then(response => response.json())
+                .then(data =>{
+                    
+            data.works.slice(0, 1).forEach((book, index) => {
+                console.log(index);
+            let coverID = book.cover_i ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` : './imagenNoDisponible.jpg';
+
+
+            let title = book.title || "Título desconocido";
+      let autorr = book.author_name && book.author_name.length > 0 ? book.author_name.join(", ") : "Desconocido";
+      console.log(autorr);
+            let publishYear = book.first_publish_year || "Desconocido";
+
+            let titulo1 = document.getElementById(`titulo${index + 1}`);
+            let aut = document.getElementById("autor1");
+            let año1 = document.getElementById(`año${index + 1}`);
+            let imagen1 = document.getElementById(`imagen${index + 1}`);
+            
+            console.log(title);
+
+
+            titulo1.innerHTML =  title ;
+            aut.innerHTML = "Autor: " + autorr;
+            año1.innerHTML = "Año : " + publishYear;
+            imagen1.src = coverID;
+
+
+        });
+
+
+        })
+}
+
+randomBooks();
