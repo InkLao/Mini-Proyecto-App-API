e0710d7f44311689537d361d9d829813439a7c79
ArturoITSON <sergio.garcia233316@potros.itson.edu.mx>
2025-04-01 12:29:53 -0700

    Mejoras en diseño

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
         
 let boton = document.getElementById("btn_consultar");
 
-boton.onclick = async function(){
+boton.onclick = function(){
  
-            let query = document.getElementById("txtBuscar").value;
-            let response = await fetch(`https://openlibrary.org/search.json?title=${query}`);
-            let data = await response.json();
-            let resultsDiv = document.getElementById("results");
-            resultsDiv.innerHTML = "";
-            
-            if (data.docs.length === 0) {
-                resultsDiv.innerHTML = "<p>No se encontraron libros</p>";
-                return;
-            }
-            
-            data.docs.slice(0, 5).forEach(book => {
-                let coverID = book.cover_i ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` : 'https://via.placeholder.com/150';
-                resultsDiv.innerHTML += `
-                    <div class="book">
-                        <img src="${coverID}" alt="Portada del libro">
-                        <div class="book-info">
-                            <h3>${book.title}</h3>
-                            <p><strong>Autor:</strong> ${book.author_name ? book.author_name.join(", ") : "Desconocido"}</p>
-                        </div>
-                    </div>
-                `;
-            });
-        }
+    let busqueda = document.getElementById("txtBuscar").value;
+    fetch(`https://openlibrary.org/search.json?q=${busqueda}`)
+    .then(response => response.json())
+    .then(data => {
+        let resultsDiv = document.getElementById("results");
 
-        
+        resultsDiv.innerHTML = ""; // Limpiar resultados previos
+        data.docs.slice(0, 5).forEach(book => {
+
+            let coverID = book.cover_i ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` : './imagenNoDisponible.jpg';
+
+            let title = book.title || "Título desconocido";
+            let author = book.author_name ? book.author_name.join(", ") : "Desconocido";
+            let publishYear = book.first_publish_year || "Desconocido";
+
+            resultsDiv.innerHTML += `
+                <div class="book">
+                    <img src="${coverID}" alt="Portada del libro">
+                    <div class="book-info">
+                        <h3>${title}</h3>
+                        <p><strong>Autor:</strong> ${author}</p>
+                        <p><strong>Año de publicación:</strong> ${publishYear}</p>
+
+                    </div>
+                </div>
+            `;
+        });
+    })
+
+} 
